013c57aac8d97c862e0e2e8495c22d4a539ad1e6
851986339@qq.com <851986339@qq.com>
2017-04-21 00:04:31 +0800

    commit

--- src/main/webapp/publicPage/assets/js/main.js
+++ src/main/webapp/publicPage/assets/js/main.js
@@ -1,6 +1,6 @@
 angular.module('app', [])
     .controller('appCtrl', ['$scope', function($scope){
-
+        //将大段文字字符串转为段落list
         var formatSpan = function (v) {
             return v.trim().split('\n');
         }
@@ -16,7 +16,7 @@ angular.module('app', [])
         //公司名
         $scope.company_name = $scope.data.cardName;
         //公司名片
-        $scope.cards = formatSpan($scope.data);
+        $scope.cards = formatSpan($scope.data.cardContent);
         $scope.notices = $scope.data.notices;
         $scope.products = $scope.data.products;
         $scope.links = $scope.data.links;
@@ -26,5 +26,29 @@ angular.module('app', [])
         }
         $scope.keydown = function(item) {
             console.log($scope.c);
+        }
+
+
+
+
+
+
+
+        //联系我们
+        $scope.contact = function () {
+            $.post('post', {
+                contact_username: $scope.contact_username,
+                contact_company: $scope.contact_company,
+                contact_phone: $scope.contact_phone,
+                contact_qq: $scope.contact_qq,
+                contact_content: $scope.contact_content,
+            }, function (data) {
+
+            });
+            console.log($scope.contact_username);
+            console.log($scope.contact_company);
+            console.log($scope.contact_phone);
+            console.log($scope.contact_qq);
+            console.log($scope.contact_content);
         }
     }]);
