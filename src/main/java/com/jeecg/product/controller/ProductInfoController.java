9d295839a5fca59aa8c6b12cb7cb8455496fa4ae
RuichaoZhang <RuichaoZhang@RuichaoZhang-PC>
2017-04-21 23:21:09 +0800

    .

--- src/main/java/com/jeecg/product/controller/ProductInfoController.java
+++ src/main/java/com/jeecg/product/controller/ProductInfoController.java
@@ -1,71 +1,67 @@
 package com.jeecg.product.controller;
-import com.jeecg.product.entity.ProductInfoEntity;
-import com.jeecg.product.service.ProductInfoServiceI;
+import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.jeecgframework.core.beanvalidator.BeanValidators;
 import org.jeecgframework.core.common.controller.BaseController;
 import org.jeecgframework.core.common.exception.BusinessException;
 import org.jeecgframework.core.common.hibernate.qbc.CriteriaQuery;
 import org.jeecgframework.core.common.model.json.AjaxJson;
 import org.jeecgframework.core.common.model.json.DataGrid;
 import org.jeecgframework.core.constant.Globals;
-import org.jeecgframework.core.util.StringUtil;
-import org.jeecgframework.tag.core.easyui.TagUtil;
-import org.jeecgframework.web.system.pojo.base.TSDepart;
-import org.jeecgframework.web.system.service.SystemService;
+import org.jeecgframework.core.util.ExceptionUtil;
 import org.jeecgframework.core.util.MyBeanUtils;
-
-import java.io.OutputStream;
-import org.jeecgframework.core.util.BrowserUtils;
-import org.jeecgframework.poi.excel.ExcelExportUtil;
+import org.jeecgframework.core.util.ResourceUtil;
+import org.jeecgframework.core.util.StringUtil;
 import org.jeecgframework.poi.excel.ExcelImportUtil;
 import org.jeecgframework.poi.excel.entity.ExportParams;
 import org.jeecgframework.poi.excel.entity.ImportParams;
-import org.jeecgframework.poi.excel.entity.TemplateExportParams;
 import org.jeecgframework.poi.excel.entity.vo.NormalExcelConstants;
-import org.jeecgframework.poi.excel.entity.vo.TemplateExcelConstants;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.jeecgframework.core.util.ResourceUtil;
-import java.io.IOException;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import java.util.Map;
-import org.jeecgframework.core.util.ExceptionUtil;
-
+import org.jeecgframework.tag.core.easyui.TagUtil;
+import org.jeecgframework.web.cgform.entity.upload.CgUploadEntity;
+import org.jeecgframework.web.cgform.service.config.CgFormFieldServiceI;
+import org.jeecgframework.web.system.service.SystemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.jeecgframework.core.beanvalidator.BeanValidators;
-import java.util.Set;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
-import java.net.URI;
-import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import org.jeecgframework.web.cgform.entity.upload.CgUploadEntity;
-import org.jeecgframework.web.cgform.service.config.CgFormFieldServiceI;
-import java.util.HashMap;
+import com.jeecg.card.entity.CardInfoEntity;
+import com.jeecg.card.service.CardInfoServiceI;
+import com.jeecg.gonggao.entity.GonggaoEntity;
+import com.jeecg.gonggao.service.GonggaoServiceI;
+import com.jeecg.link.entity.LinkageinfoEntity;
+import com.jeecg.link.service.LinkageinfoServiceI;
+import com.jeecg.product.entity.Company;
+import com.jeecg.product.entity.ProductInfoEntity;
+import com.jeecg.product.service.ProductInfoServiceI;
 /**   
  * @Title: Controller  
  * @Description: 公司产品表
@@ -90,6 +86,12 @@ public class ProductInfoController extends BaseController {
 	private Validator validator;
 	@Autowired
 	private CgFormFieldServiceI cgFormFieldService;
+	@Autowired
+	private CardInfoServiceI cardInfoService;
+	@Autowired
+	private GonggaoServiceI gonggaoService;
+	@Autowired
+	private LinkageinfoServiceI linkService;
 	
 
 
@@ -432,5 +434,40 @@ public class ProductInfoController extends BaseController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") String id) {
 		productInfoService.deleteEntityById(ProductInfoEntity.class, id);
+	}
+	@RequestMapping(params = "publishInfo")
+	@ResponseStatus(HttpStatus.OK)
+	public void publishInfo(HttpServletRequest request,HttpServletResponse response
+			, DataGrid dataGrid,ModelMap modelMap) {
+		CardInfoEntity cardInfoEntity = (CardInfoEntity) cardInfoService.findByQueryString("from CardInfoEntity").get(0);
+		GonggaoEntity gonggaoEntity = (GonggaoEntity) gonggaoService.findByQueryString("from GonggaoEntity").get(0);
+		List<LinkageinfoEntity> linkageinfoEntities = linkService.findByQueryString("from LinkageinfoEntity");
+		List<ProductInfoEntity> productInfoEntities = productInfoService.findByQueryString("from ProductInfoEntity");
+		ObjectMapper mapper = new ObjectMapper();
+		Company t = new Company();
+		t.setCardInfoEntity(cardInfoEntity);
+		t.setLinkageinfoEntities(linkageinfoEntities);
+		t.setProducts(productInfoEntities);
+		t.setGonggaoEntity(gonggaoEntity);
+		String json = null;
+        try {
+			json=mapper.writeValueAsString(t);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+        try {
+			response.getWriter().append(json);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
 	}
 }
